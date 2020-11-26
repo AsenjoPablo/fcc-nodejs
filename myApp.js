9f8b3c0defd16aef8044bf13f8d2b7bf26eeb0f6
Pablo <pasenjo94@gmail.com>
2020-11-26 12:08:13 +0100

    added ex2

--- myApp.js
+++ myApp.js
@@ -1,39 +1,16 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+app.listen(8888);
 
+app.get("/", function (req, res) {
+  // mediante res.send() podemos devolver una cadena:
+  // res.send("Hola mundo desde res.send()");
 
+  // sendFile devuelve un fichero.
+  // __dirname es una buena práctica, indica el directorio actual (de app.js en este caso)
+  // y después simplemente añadimos el resto de la ruta ("/views/index.html")
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
